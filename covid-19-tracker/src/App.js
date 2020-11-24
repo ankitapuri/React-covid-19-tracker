7435c61b03d053f608364110c599a12d6c1c9572
Ankita Puri <54734002+ankitapuri@users.noreply.github.com>
2020-11-24 06:03:54 +0000

    added

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import './App.css';
-import {
-  MenuItem,
-  FormControl,
-  Select,
-  Card,
-  CardContent,
-} from "@material-ui/core";
+import { MenuItem, FormControl, Select, Card,CardContent } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+     const getCountriesData = async () => {
+       await fetch("https://disease.sh/v3/covid-19/countries")
+       .then((response) => response.json())
+       .then((data) => {
+         const countries = data.map((country) => (
+           {
+             name:country.country,
+             value:country.countryInfo.iso2
+           }));
+           setCountries(countries);
+       });
+     };
+     getCountriesData();
+  },[]);
   return (
     <div className="app">
       <div className="app__header">
@@ -18,10 +29,15 @@ function App() {
           variant="outlined"
           value="abc"
           >
+            {
+              countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+            {/*<MenuItem value="worldwide">Worldwide</MenuItem>
             <MenuItem value="worldwide">Worldwide</MenuItem>
             <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>*/}
 
           </Select>
         </FormControl>
