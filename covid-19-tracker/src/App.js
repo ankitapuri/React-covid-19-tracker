c9ebef6e7522d14ef663326e390e2c614de09c3c
Ankita Puri <54734002+ankitapuri@users.noreply.github.com>
2020-11-24 12:24:15 +0000

    added

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -2,11 +2,21 @@ import React, {useState,useEffect} from "react";
 import './App.css';
 import { MenuItem, FormControl, Select, Card,CardContent } from "@material-ui/core";
 import InfoBox from './InfoBox';
+import Map from './Map.js'
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
 
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then(response => response.json())
+    .then(data =>{
+      setCountryInfo(data);
+    });
+  },[]);
+   
   useEffect(() => {
      const getCountriesData = async () => {
        await fetch("https://disease.sh/v3/covid-19/countries")
@@ -23,12 +33,22 @@ function App() {
      getCountriesData();
   },[]);
 
-  const onCountryChange = (event) => {
+  const onCountryChange = async (event) => {
     const countryCode = event.target.value;
     setCountry(countryCode);
-  }
+
+    const url = countryCode === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : 
+    `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setCountry(countryCode);
+      setCountryInfo(data);
+    })
+  };
   return (
     <div className="app">
+      <div className="app__left">
       <div className="app__header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="app__dropdown">
@@ -49,10 +69,22 @@ function App() {
         </FormControl>
       </div>
       <div className="app__stats">
-      <InfoBox title="coronavirus cases" cases={123} total={2000} />
-      <InfoBox title="Recovered" cases={1235} total={5000} />
-      <InfoBox title="Deaths" cases={12376} total={4000} />
+      <InfoBox title="coronavirus cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+      <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+      <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
       </div>
+      <Map />
+      </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live cases by country</h3>
+          {/*Table*/}
+          <h3>Worldwide new cases</h3>
+         {/*Graph*/}
+         </CardContent>
+        
+      </Card>
+      
     </div>
   );
 }
---
+++ covid-19-tracker/src/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Map() {
+    return (
+        <div className="map">
+         <h1>I am a Map</h1>   
+        </div>
+    );
+}
+
+export default Map;
